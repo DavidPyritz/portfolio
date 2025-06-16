36d3b00430c09a3689ad6c7b07b6c2b1b0264870
David Pyritz <david.pyritz@gmail.com>
2025-06-17 00:07:41 +0200

    structure update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from "./header/header.component";
+import { HeroSectionComponent } from "./hero-section/hero-section.component";
+import { WhyMeSectionComponent } from "./why-me-section/why-me-section.component";
+import { MySkillSectionComponent } from "./my-skill-section/my-skill-section.component";
+import { ContactSectionComponent } from "./contact-section/contact-section.component";
+import { ReferencesComponent } from "./references/references.component";
+import { MenuBarComponent } from "./menu-bar/menu-bar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, HeroSectionComponent, WhyMeSectionComponent, MySkillSectionComponent, ContactSectionComponent, ReferencesComponent, MenuBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/my-skill-section/my-skill-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-skill-section',
+  imports: [],
+  templateUrl: './my-skill-section.component.html',
+  styleUrl: './my-skill-section.component.scss'
+})
+export class MySkillSectionComponent {
+
+}
